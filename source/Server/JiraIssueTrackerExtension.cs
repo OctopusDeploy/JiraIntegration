9dc83b76f10bae633c0755796c8b87f797f0399d
Shannon Lewis <slewis74@gmail.com>
2019-01-18 17:04:59 +1000

    making WorkItemPackage a server concern

--- source/Server/JiraIssueTrackerExtension.cs
+++ source/Server/JiraIssueTrackerExtension.cs
@@ -4,11 +4,9 @@ using Octopus.Server.Extensibility.Extensions;
 using Octopus.Server.Extensibility.Extensions.Infrastructure;
 using Octopus.Server.Extensibility.Extensions.Infrastructure.Configuration;
 using Octopus.Server.Extensibility.Extensions.Mappings;
-using Octopus.Server.Extensibility.Extensions.Model.Projects;
 using Octopus.Server.Extensibility.IssueTracker.Extensions;
 using Octopus.Server.Extensibility.IssueTracker.Jira.Configuration;
 using Octopus.Server.Extensibility.IssueTracker.Jira.Deployments;
-using Octopus.Server.Extensibility.IssueTracker.Jira.Model.Projects;
 
 namespace Octopus.Server.Extensibility.IssueTracker.Jira
 {
@@ -32,11 +30,6 @@ namespace Octopus.Server.Extensibility.IssueTracker.Jira
                 .As<IHasConfigurationSettings>()
                 .As<IHasConfigurationSettingsResource>()
                 .As<IContributeMappings>()
-                .InstancePerLifetimeScope();
-
-            builder.RegisterType<ProjectSettings>()
-                .As<IContributeProjectSettingsMetadata>()
-                .As<IContributeWorkItemsToReleases>()
                 .InstancePerLifetimeScope();
 
             builder.RegisterType<JiraIssueTracker>()
