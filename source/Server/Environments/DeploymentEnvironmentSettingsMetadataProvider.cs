6b388b23419247e582e371f57441a5f70be2487f
Shannon Lewis <slewis74@gmail.com>
2019-02-01 13:31:37 +1000

    fixed the JiraEnvironmentType to be an option selector

--- source/Server/Environments/DeploymentEnvironmentSettingsMetadataProvider.cs
+++ source/Server/Environments/DeploymentEnvironmentSettingsMetadataProvider.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using Octopus.Data.Resources.Attributes;
 using Octopus.Server.Extensibility.Extensions.Model.Environments;
 using Octopus.Server.Extensibility.IssueTracker.Jira.Configuration;
 using Octopus.Server.Extensibility.Metadata;
@@ -28,6 +29,7 @@ namespace Octopus.Server.Extensibility.IssueTracker.Jira.Environments
             [DisplayName("Jira Environment Type")]
             [Description("The Jira environment type of this Octopus deployment environment.")]
             [ReadOnly(false)]
+            [HasOptions(SelectMode.Single)]
             public JiraEnvironmentType JiraEnvironmentType { get; set; }
         }
     }
