d3c2194cdae921038564602d08c507976816aac0
Henrik Andersson <henrik.andersson@octopus.com>
2019-03-12 11:45:46 +1000

    Update server extensibility reference

--- source/Server/Deployments/DeploymentObserver.cs
+++ source/Server/Deployments/DeploymentObserver.cs
@@ -58,7 +58,7 @@ namespace Octopus.Server.Extensibility.IssueTracker.Jira.Deployments
 
         public void Handle(DeploymentEvent domainEvent)
         {
-            if (!store.GetIsEnabled() || domainEvent.Deployment.DeploymentReleaseNotes.All(drn => drn.VersionMetadata.All(pm => pm.OctopusPackageMetadata.IssueTrackerId != JiraConfigurationStore.SingletonId)))
+            if (!store.GetIsEnabled() || domainEvent.Deployment.Changes.All(drn => drn.VersionMetadata.All(pm => pm.OctopusPackageMetadata.IssueTrackerId != JiraConfigurationStore.SingletonId)))
                 return;
 
             using (log.OpenBlock($"Sending Jira state update - {StateFromEventType(domainEvent.EventType)}"))
@@ -141,7 +141,7 @@ namespace Octopus.Server.Extensibility.IssueTracker.Jira.Deployments
                             DeploymentSequenceNumber = int.Parse(deployment.Id.Split('-')[1]),
                             UpdateSequenceNumber = DateTime.UtcNow.Ticks,
                             DisplayName = serverTask.Description,
-                            IssueKeys = deployment.DeploymentReleaseNotes.SelectMany(drn => drn.VersionMetadata
+                            IssueKeys = deployment.Changes.SelectMany(drn => drn.VersionMetadata
                                 .Where(pm => pm.OctopusPackageMetadata.IssueTrackerId == JiraConfigurationStore.SingletonId)
                                 .SelectMany(pm => pm.OctopusPackageMetadata.WorkItems)
                                 .Select(wi => wi.Id)
