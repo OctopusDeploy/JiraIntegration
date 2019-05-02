d798e3916cb5827acee89d1524e4e23993617086
Henrik Andersson <henrik.andersson@octopus.com>
2019-05-02 13:54:45 +1000

    Adjust regex to cover a few more cases and add tests for those cases

--- source/Server/WorkItems/CommentParser.cs
+++ source/Server/WorkItems/CommentParser.cs
@@ -6,8 +6,18 @@ namespace Octopus.Server.Extensibility.IssueTracker.Jira.WorkItems
 {
     public class CommentParser
     {
-        // Expression based on example found here https://confluence.atlassian.com/stashkb/integrating-with-custom-jira-issue-key-313460921.html?_ga=2.163394108.1696841245.1556699049-1954949426.1532303954
-        private static readonly Regex Expression = new Regex("((?<!([A-Z0-9.]{1,10})-?)[A-Z0-9]+-\\d+)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        // Expression based on example found here https://confluence.atlassian.com/stashkb/integrating-with-custom-jira-issue-key-313460921.html?_ga=2.163394108.1696841245.1556699049-1954949426.1532303954 with modified negative lookbehind
+        // Explanation:
+        // Negative Lookbehind (?<!([\041-\053\056-\176]{1,10})-?)
+        // Assert that the Regex below does not match
+        //  2nd Capturing Group ([\041-\053\056-\176]{1,10})
+        //   Match a single character present in the list below [\041-\053\056-\176]{1,10}
+        //   {1,10} Quantifier — Matches between 1 and 10 times, as many times as possible, giving back as needed (greedy)
+        //   \041-\053 a single character in the range between ! (index 33) and + (index 43) (case insensitive)
+        //   \056-\176 a single character in the range between . (index 46) and ~ (index 126) (case insensitive)
+        //  -? matches the character - literally (case insensitive)
+        //   ? Quantifier — Matches between zero and one times, as many times as possible, giving back as needed (greedy)
+        private static readonly Regex Expression = new Regex("((?<!([\\041-\\053\\056-\\176]{1,10})-?)[A-Z0-9]+-\\d+)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
         
         public string[] ParseWorkItemIds(OctopusPackageMetadata packageMetadata)
         {
