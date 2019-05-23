e5816b72cdc0bef78c2ea637eeb5d1e1dc4dd58e
Shannon Lewis <slewis74@gmail.com>
2019-05-23 20:48:50 +1000

    updates so that the cloud only config fields aren't applicable, and thus hidden in the UI, when the users tell us they are using Jira Server

--- source/Server/Configuration/JiraConfiguration.cs
+++ source/Server/Configuration/JiraConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using Octopus.Server.Extensibility.Extensions.Infrastructure.Configuration;
+﻿using Octopus.Data.Resources.Attributes;
+using Octopus.Server.Extensibility.Extensions.Infrastructure.Configuration;
 using Octopus.Shared.Model;
 
 namespace Octopus.Server.Extensibility.IssueTracker.Jira.Configuration
@@ -9,13 +10,25 @@ namespace Octopus.Server.Extensibility.IssueTracker.Jira.Configuration
         {
             Id = JiraConfigurationStore.SingletonId;
             ConnectAppUrl = "https://jiraconnectapp.octopus.com";
+            JiraInstanceType = JiraInstanceType.Cloud;
         }
 
+        public JiraInstanceType JiraInstanceType { get; set; }
+        
         public string BaseUrl { get; set; }
+        
         [Encrypted]
         public string Password { get; set; }
+
         public string ConnectAppUrl { get; set; }
+        
         public ReleaseNoteOptions ReleaseNoteOptions { get; set; } = new ReleaseNoteOptions();
+    }
+
+    public enum JiraInstanceType
+    {
+        Cloud,
+        Server
     }
 
     public class ReleaseNoteOptions
