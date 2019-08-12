e3dcf1dea004426749988ebc58bd20604c6caa8a
Henrik Andersson <henrik.andersson@octopus.com>
2019-08-12 15:33:45 +1000

    Add connectivity test buttons to Jira config page

--- source/Client/JiraConfigurationResource.cs
+++ source/Client/JiraConfigurationResource.cs
@@ -8,6 +8,7 @@ namespace Octopus.Client.Extensibility.IssueTracker.Jira
     public class JiraConfigurationResource : ExtensionConfigurationResource
     {
         public const string JiraBaseUrlDescription = "Set the base url for the Jira instance.";
+        public const string ApiConnectAppCredentialsTest = "/api/jiraissuetracker/connectivitycheck/connectapp";
 
         public JiraConfigurationResource()
         {
@@ -22,6 +23,7 @@ namespace Octopus.Client.Extensibility.IssueTracker.Jira
         [DisplayName("Jira Connect App Password")]
         [Description("Set the password for authenticating with the Jira Connect App")]
         [Writeable]
+        [AllowConnectivityCheck("Jira Connect App credentials", ApiConnectAppCredentialsTest, nameof(BaseUrl), nameof(Password))]
         public SensitiveValue Password { get; set; }
 
         [DisplayName("Octopus Installation Id")]
@@ -38,6 +40,7 @@ namespace Octopus.Client.Extensibility.IssueTracker.Jira
         public const string UsernameDescription = "Set the username to authenticate with against your Jira instance.";
         public const string PasswordDescription = "Set the password to authenticate with against your Jira instance.";
         public const string ReleaseNotePrefixDescription = "Set the prefix to look for when finding release notes for Jira issues. For example `Release note:`.";
+        public const string ApiJiraCredentialsTest = "/api/jiraissuetracker/connectivitycheck/jira";
 
         [DisplayName("Jira Username")]
         [Description(UsernameDescription)]
@@ -47,6 +50,7 @@ namespace Octopus.Client.Extensibility.IssueTracker.Jira
         [DisplayName("Jira Password")]
         [Description(PasswordDescription)]
         [Writeable]
+        [AllowConnectivityCheck("Jira credentials", ApiJiraCredentialsTest, nameof(JiraConfigurationResource.BaseUrl), nameof(Username), nameof(Password))]
         public SensitiveValue Password { get; set; }
 
         [DisplayName("Release Note Prefix")]
