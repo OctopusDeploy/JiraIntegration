4a257c9ce4e419f411b31dfc364e55e7a0c997a7
Reuben Unruh <reuben.unruh@sa.ucsb.edu>
2019-08-27 13:41:56 -0700

    Ignore Jira refs followed by alpha chars and simplify

--- source/Server/WorkItems/CommentParser.cs
+++ source/Server/WorkItems/CommentParser.cs
@@ -8,7 +8,7 @@ namespace Octopus.Server.Extensibility.IssueTracker.Jira.WorkItems
     {
         // Expression based on example found here https://confluence.atlassian.com/stashkb/integrating-with-custom-jira-issue-key-313460921.html?_ga=2.163394108.1696841245.1556699049-1954949426.1532303954 with modified negative lookbehind
         // with added '$' and '.' to exclude strings that look similar to Jira issues, e.g. `text that may cause confusion: $foo-1 or test.TST-01.com`
-        private static readonly Regex Expression = new Regex("((?<!([A-Z0-9\\$\\.]{1,10})-?)[A-Z0-9]+-\\d+)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        private static readonly Regex Expression = new Regex("(?<![A-Z0-9\\$\\.]-?)(?>[A-Z0-9]+-\\d+)(?![A-Z])", RegexOptions.Compiled | RegexOptions.IgnoreCase);
         
         public string[] ParseWorkItemIds(OctopusPackageMetadata packageMetadata)
         {
