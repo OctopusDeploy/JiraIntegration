ebd4cb4f724f88b037ffaa93bb6e3f802121559c
Shannon Lewis <slewis74@gmail.com>
2019-10-17 20:33:08 +1000

    fix for tests

--- source/Server/Properties/AssemblyInfo.cs
+++ source/Server/Properties/AssemblyInfo.cs
@@ -2,3 +2,4 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Octopus.Server.Extensibility.IssueTracker.Jira.Tests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
