c1f1f3acac1c880ddd4e278a950ba7cf7161eb78
Shannon Lewis <slewis74@gmail.com>
2019-10-31 20:09:36 +1000

    got too carried away, the client resources should be public still

--- source/Client/JiraConfigurationResource.cs
+++ source/Client/JiraConfigurationResource.cs
@@ -5,7 +5,7 @@ using Octopus.Client.Model;
 
 namespace Octopus.Client.Extensibility.IssueTracker.Jira
 {
-    class JiraConfigurationResource : ExtensionConfigurationResource
+    public class JiraConfigurationResource : ExtensionConfigurationResource
     {
         public const string JiraBaseUrlDescription = "Set the base url for the Jira instance.";
 
@@ -33,7 +33,7 @@ namespace Octopus.Client.Extensibility.IssueTracker.Jira
         public ReleaseNoteOptionsResource ReleaseNoteOptions { get; set; } = new ReleaseNoteOptionsResource();
     }
 
-    class ReleaseNoteOptionsResource
+    public class ReleaseNoteOptionsResource
     {
         public const string UsernameDescription = "Set the username to authenticate with against your Jira instance.";
         public const string PasswordDescription = "Set the password to authenticate with against your Jira instance.";
