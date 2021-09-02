bdad93c0b64edafd4ed8ac58311628e3c2b2d0a0
hnrkndrssn <henrik@henrikandersson.org>
2021-09-03 08:36:15 +1000

    Add e2e tests for integrating with Jira

--- source/Server/Properties/AssemblyInfo.cs
+++ source/Server/Properties/AssemblyInfo.cs
@@ -2,4 +2,5 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Octopus.Server.Extensibility.JiraIntegration.Tests")]
+[assembly: InternalsVisibleTo("Octopus.Server.Extensibility.JiraIntegration.E2E.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
