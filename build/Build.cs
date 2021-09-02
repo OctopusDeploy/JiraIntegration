94935211cb2f59cbd7716c2d922ab8f9963c0597
hnrkndrssn <henrik@henrikandersson.org>
2021-09-02 11:51:25 +1000

    Move to OctopusNuke component

--- build/Build.cs
+++ build/Build.cs
@@ -1,139 +1,38 @@
+// ReSharper disable RedundantUsingDirective
+
+using System;
 using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.Execution;
+using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.Tools.DotNet.DotNetTasks;
 using Nuke.Common.Tools.OctoVersion;
+using Octopus.NukeBuildComponents;
+using Octopus.NukeBuildComponents.Build;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [CheckBuildProjectConfigurations]
-[UnsetVisualStudioEnvironmentVariables]
-class Build : NukeBuild
+[ShutdownDotNetAfterServerBuild]
+class Build : NukeBuild, IExtensionBuild
 {
-    readonly Configuration Configuration = Configuration.Release;
+    public string TargetPackageDescription => "JiraIntegration";
 
-    [Solution] readonly Solution Solution;
-
-    [OctoVersion] readonly OctoVersionInfo OctoVersionInfo;
-
-    static AbsolutePath SourceDirectory => RootDirectory / "source";
-    static AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
-    static AbsolutePath PublishDirectory => RootDirectory / "publish";
-    static AbsolutePath LocalPackagesDir => RootDirectory / ".." / "LocalPackages";
-
-    Target Clean => _ => _
-        .Before(Restore)
-        .Executes(() =>
-        {
-            SourceDirectory.GlobDirectories("**/bin", "**/obj", "**/TestResults").ForEach(DeleteDirectory);
-            EnsureCleanDirectory(ArtifactsDirectory);
-            EnsureCleanDirectory(PublishDirectory);
-        });
-
-    Target CalculateVersion => _ => _
-        .Executes(() =>
-        {
-            //all the magic happens inside `[NukeOctoVersion]` above. we just need a target for TeamCity to call
-        });
-
-    Target Restore => _ => _
-        .DependsOn(Clean)
-        .Executes(() =>
-        {
-            DotNetRestore(_ => _
-                .SetProjectFile(Solution));
-        });
-
-    Target Compile => _ => _
-        .DependsOn(Clean)
-        .DependsOn(Restore)
-        .Executes(() =>
-        {
-            Logger.Info("Building Jira Integration v{0}", OctoVersionInfo.FullSemVer);
-
-            DotNetBuild(_ => _
-                .SetProjectFile(Solution)
-                .SetConfiguration(Configuration)
-                .SetVersion(OctoVersionInfo.FullSemVer)
-                .EnableNoRestore());
-        });
-
-    Target Test => _ => _
-        .DependsOn(Compile)
-        .Executes(() =>
-        {
-            DotNetTest(_ => _
-                .SetProjectFile(Solution)
-                .SetConfiguration(Configuration)
-                .SetNoBuild(true)
-                .EnableNoRestore()
-                .SetFilter(@"FullyQualifiedName\!~Integration.Tests"));
-        });
-
-    Target Pack => _ => _
-        .DependsOn(Test)
-        .Produces(ArtifactsDirectory / "*.nupkg")
-        .Executes(() =>
-        {
-            Logger.Info("Packing Jira Integration v{0}", OctoVersionInfo.FullSemVer);
-
-            DotNetPack(_ => _
-                .SetProject(Solution)
-                .SetVersion(OctoVersionInfo.FullSemVer)
-                .SetConfiguration(Configuration)
-                .SetOutputDirectory(ArtifactsDirectory)
-                .EnableNoBuild()
-                .DisableIncludeSymbols()
-                .SetVerbosity(DotNetVerbosity.Normal)
-                .SetProperty("NuspecFile", "../../build/Octopus.Server.Extensibility.JiraIntegration.nuspec")
-                .SetProperty("NuspecProperties", $"Version={OctoVersionInfo.FullSemVer}"));
-
-            DotNetPack(_ => _
-                .SetProject(RootDirectory / "source/Client/Client.csproj")
-                .SetVersion(OctoVersionInfo.FullSemVer)
-                .SetConfiguration(Configuration)
-                .SetOutputDirectory(ArtifactsDirectory)
-                .EnableNoBuild()
-                .DisableIncludeSymbols()
-                .SetVerbosity(DotNetVerbosity.Normal)
-                .SetProperty("NuspecProperties", $"Version={OctoVersionInfo.FullSemVer}"));
-        });
-
-    Target CopyToLocalPackages => _ => _
-        .OnlyWhenStatic(() => IsLocalBuild)
-        .TriggeredBy(Pack)
-        .Executes(() =>
-        {
-            EnsureExistingDirectory(LocalPackagesDir);
-            ArtifactsDirectory.GlobFiles("*.nupkg")
-                .ForEach(package =>
-                {
-                    CopyFileToDirectory(package, LocalPackagesDir);
-                });
-        });
-
-    Target OutputPackagesToPush => _ => _
-        .DependsOn(Pack)
-        .Executes(() =>
-        {
-            var artifactPaths = ArtifactsDirectory.GlobFiles("*.nupkg")
-                .NotEmpty()
-                .Select(p => p.ToString());
-
-            System.Console.WriteLine($"::set-output name=packages_to_push::{string.Join(',', artifactPaths)}");
-        });
-
-    Target Default => _ => _
-        .DependsOn(OutputPackagesToPush);
-
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    public Enumeration Config => Configuration.Release;
     /// Support plugins are available for:
     /// - JetBrains ReSharper        https://nuke.build/resharper
     /// - JetBrains Rider            https://nuke.build/rider
     /// - Microsoft VisualStudio     https://nuke.build/visualstudio
     /// - Microsoft VSCode           https://nuke.build/vscode
-    public static int Main() => Execute<Build>(x => x.Default);
+    public static int Main() => Execute<Build>(x => ((IExtensionBuild)x).Default);
+
+    public string NuspecFilePath => "../../build/Octopus.Server.Extensibility.JiraIntegration.nuspec";
 }
