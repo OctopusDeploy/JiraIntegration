62a993868d3da50dfb238ff531d97ebafa6c18cc
slewis74 <slewis74@gmail.com>
2022-04-06 11:05:09 +1000

    Added handling for Http 401/403 errors back from Jira, to help troubleshoot things like expired/revoked API keys/access tokens

---
+++ source/Server.E2E.Tests/WorkItemLinkMapperErrorHandlingTestScript.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using NSubstitute;
+using NUnit.Framework;
+using Octopus.Diagnostics;
+using Octopus.Server.Extensibility.JiraIntegration.Integration;
+using Octopus.Server.Extensibility.JiraIntegration.WorkItems;
+using Octopus.Server.Extensibility.Results;
+using Octopus.Server.MessageContracts.Features.BuildInformation;
+using Octopus.Server.MessageContracts.Features.IssueTrackers;
+using Shouldly;
+
+namespace Octopus.Server.Extensibility.JiraIntegration.E2E.Tests
+{
+    [TestFixture]
+    public class WorkItemLinkMapperErrorHandlingTestScript : WorkItemMapperBaseFixture
+    {
+        [Test]
+        public void HttpAuthExceptionShouldLogMeaningfulMessage()
+        {
+            if(!TryGetJiraSettings(out var baseUrl, out var username, out var _))
+                Assert.Ignore($"Configure the following environment variables '{JiraBaseUrlEnvironmentVariable}', '{JiraUsernameEnvironmentVariable}', '{JiraAuthTokenEnvironmentVariable}' to run these tests.");
+
+            var log = Substitute.For<ISystemLog>();
+
+            var store = BuildJiraConfigurationStore(baseUrl, username, "invalidtoken");
+            var jira = BuildJiraRestClient(baseUrl, username, "invalidtoken", log);
+
+            var buildInformation = CreateBuildInformation(new[]
+            {
+                new Commit
+                {
+                    Id = "234",
+                    Comment = "OATP-9999"
+                }
+            });
+
+            var workItemLinkMapper = new WorkItemLinkMapper(
+                store,
+                new CommentParser(),
+                new Lazy<IJiraRestClient>(jira),
+                log);
+
+            IResultFromExtension<WorkItemLink[]> result = null;
+            Should.NotThrow(
+                () =>
+                {
+                    result = workItemLinkMapper.Map(buildInformation);
+                });
+
+            result.ShouldBeOfType<FailureResultFromExtension<WorkItemLink[]>>("A failure should be received");
+            var failure = (FailureResultFromExtension<WorkItemLink[]>)result;
+            failure.ErrorString.ShouldStartWith("Authentication failure, check the Jira access token is valid and has permissions to read work items");
+        }
+    }
+}
---
+++ source/Server.E2E.Tests/WorkItemMapperBaseFixture.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using NSubstitute;
+using Octopus.CoreUtilities.Extensions;
+using Octopus.Data.Model;
+using Octopus.Diagnostics;
+using Octopus.Server.Extensibility.Extensions.Infrastructure.Web.Api;
+using Octopus.Server.Extensibility.JiraIntegration.Configuration;
+using Octopus.Server.Extensibility.JiraIntegration.Integration;
+using Octopus.Server.MessageContracts.Features.BuildInformation;
+
+namespace Octopus.Server.Extensibility.JiraIntegration.E2E.Tests
+{
+    public class WorkItemMapperBaseFixture
+    {
+        protected const string JiraBaseUrlEnvironmentVariable = "JiraIntegration_E2E_BaseUrl";
+        protected const string JiraUsernameEnvironmentVariable = "JiraIntegration_E2E_Username";
+        protected const string JiraAuthTokenEnvironmentVariable = "JiraIntegration_E2E_AuthToken";
+
+        internal static JiraRestClient BuildJiraRestClient(string baseUrl, string username, string authToken, ISystemLog log)
+        {
+            var httpClientFactory = BuildOctopusHttpClientFactory(baseUrl, username, authToken);
+
+            return new JiraRestClient(
+                baseUrl,
+                username,
+                authToken,
+                log,
+                httpClientFactory);
+        }
+
+        static IOctopusHttpClientFactory BuildOctopusHttpClientFactory(string baseUrl, string username, string authToken)
+        {
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(baseUrl)
+            };
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+                Convert.ToBase64String(Encoding.UTF8.GetBytes($"{username}:{authToken}")));
+
+            var httpClientFactory = Substitute.For<IOctopusHttpClientFactory>();
+            httpClientFactory.CreateClient().Returns(httpClient);
+
+            return httpClientFactory;
+        }
+
+        internal static IJiraConfigurationStore BuildJiraConfigurationStore(string baseUrl, string username, string authToken, bool isEnabled = true, string releaseNotePrefix = "Release note:")
+        {
+            var store = Substitute.For<IJiraConfigurationStore>();
+            store.GetIsEnabled().Returns(isEnabled);
+            store.GetJiraUsername().Returns(username);
+            store.GetJiraPassword().Returns(authToken.ToSensitiveString());
+            store.GetBaseUrl().Returns(baseUrl);
+            store.GetReleaseNotePrefix().Returns(releaseNotePrefix);
+            return store;
+        }
+
+        protected static bool TryGetJiraSettings(out string jiraBaseUrl, out string jiraUsername, out string jiraAuthToken)
+        {
+            jiraBaseUrl = Environment.GetEnvironmentVariable(JiraBaseUrlEnvironmentVariable);
+            jiraUsername = Environment.GetEnvironmentVariable(JiraUsernameEnvironmentVariable);
+            jiraAuthToken = Environment.GetEnvironmentVariable(JiraAuthTokenEnvironmentVariable);
+
+            return !jiraBaseUrl.IsNullOrEmpty() &&
+                !jiraUsername.IsNullOrEmpty() &&
+                !jiraAuthToken.IsNullOrEmpty();
+        }
+
+        internal static OctopusBuildInformation CreateBuildInformation(Commit[] commits)
+        {
+            return new OctopusBuildInformation
+            {
+                VcsType = "Git",
+                VcsRoot = "https://github.com/testOrg/testRepo",
+                Branch = "main",
+                BuildEnvironment = "buildserverX",
+                Commits = commits
+            };
+        }
+    }
+}
